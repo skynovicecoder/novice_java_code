cbc00c8aa19d15f72e1f8252ce89c69b2be265be
SONIL KUMAR <sonil@SONILs-MacBook-Air.local>
2025-09-23 23:51:18 +0530

    longestNonRepeatingCharacters

---
+++ practice/slidingwindows/LongestRepeatingCharacters.java
@@ -0,0 +1,44 @@
+package practice.slidingwindows;
+
+import java.util.HashMap;
+
+public class LongestRepeatingCharacters {
+
+    public static int longestRepeatingCharactersLength(String input, int allowedReplacements) {
+        HashMap<Character, Integer> alphabetMap = new HashMap<>();
+        int res = 0;
+
+        int l = 0, maxFrequency = 0;
+        //int l = 0;
+        for (int r = 0; r < input.length(); r++) {
+            alphabetMap.put(input.charAt(r), alphabetMap.getOrDefault(input.charAt(r), 0) + 1);
+            maxFrequency = Math.max(maxFrequency, alphabetMap.get(input.charAt(r)));
+            //int maxFrequency  = maxCount(count);
+
+            while ((r - l + 1) - maxFrequency > allowedReplacements) {
+                alphabetMap.put(input.charAt(l), alphabetMap.get(input.charAt(l)) - 1);
+                l++;
+            }
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+
+    /*public static int maxCount(Map<Character, Integer> map) {
+        return map.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getValue)
+                .orElseGet(() -> 0);
+    }*/
+
+    public static void main(String[] args) {
+        String input = "XYYX";
+        int allowedReplacements = 2;
+        System.out.println(longestRepeatingCharactersLength(input, allowedReplacements));
+
+        String input1 = "AAABABB";
+        int allowedReplacements1 = 1;
+        System.out.println(longestRepeatingCharactersLength(input1, allowedReplacements1));
+    }
+}
