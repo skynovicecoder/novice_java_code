c525fb73dc7afef552bcee319af8977a764e34e4
SONIL KUMAR <sonil@SONILs-MacBook-Air.local>
2025-10-17 23:52:39 +0530

    dp problem for minium coins

--- practice/dp/coins/CoinsSum.java
+++ practice/dp/coins/CoinsSum.java
@@ -4,25 +4,35 @@ import java.util.Arrays;
 
 public class CoinsSum {
 
-    public static int minCoins(int[] coins, int totalSum){
-        int[] dp = new int[totalSum+1];
-        Arrays.fill(dp, totalSum+1);
-        dp[0]=0;
+    public static int minCoins(int[] coins, int totalSum) {
+        int[] dp = new int[totalSum + 1];
+        Arrays.fill(dp, totalSum + 1);
+        dp[0] = 0;
 
-        for(int i =1; i<=totalSum; i++){
+        /*for(int i =1; i<=totalSum; i++){
             for(int coin: coins){
                 if(i-coin>=0){
                     dp[i]=Math.min(dp[i], dp[i-coin]+1);
                 }
             }
+        }*/
+
+        for (int coin : coins) {
+            for (int i = coin; i <= totalSum; i++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
         }
 
-        return dp[totalSum]>totalSum ? -1: dp[totalSum];
+        return dp[totalSum] > totalSum ? -1 : dp[totalSum];
     }
 
     public static void main(String[] args) {
         int[] coins = {1, 4, 5};
         int amount = 13;
         System.out.println("Minimum coins needed: " + minCoins(coins, amount)); // Output: 3
+
+        int[] coinsArray1 = {1, 3, 4, 5};
+        int amountReq1 = 7;
+        System.out.println("Minimum coins needed: " + minCoins(coinsArray1, amountReq1)); // Output: 2
     }
 }
